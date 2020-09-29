12f77847934d84052c2ddcfc18926976daa6cae0
Autum Perconti <autumlucille@gmail.com>
2020-09-28 21:48:18 -0700

    adding search

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,54 +1,58 @@
 // Get a reference to the table body
 const tbody = d3.select("tbody");
 
-// // Console.log the UFO data from data.js
-// console.log(data);
-
-// // Step 1: Loop Through `data` and console.log each UFO report object
-// data.forEach(data => console.log(data));
-
-// // // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(data=> {
-//   console.log(data);
-//   let row = tbody.append("tr");
-// });
-
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(data=> {
-//   let row = tbody.append("tr");
-//   console.log(Object.values(data))
-//   console.log(Object.entries(data))
-// });
-
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(data=> {
-//   let row = tbody.append("tr");
-//   Object.values(data).forEach(value => {
-//     console.log(value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     let cell = row.append("td");
-//   });
-// });
-
-// Step 5: Use d3 to update each cell's text with
+//Use d3 to update each cell's text with
 // weather report values (weekday, date, high, low)
 data.forEach(data => {
-  let row = tbody.append("tr");
-  Object.values(data).forEach(value => {
-    // Append a cell to the row for each value
-    // in the weather report object
-    var cell = row.append("td");
-    cell.text(value);
-  });
+    let row = tbody.append("tr");
+    Object.values(data).forEach(value => {
+        // Append a cell to the row for each value
+        // in the weather report object
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
 
-// // BONUS: Refactor to use Arrow Functions!
-// data.forEach((weatherReport) => {
-//     let row = tbody.append("tr");
-//     Object.values(weatherReport).forEach(value => {
-//         let cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
+//JavaScript that listends for events through the date/time column to find rows that match user input.
+// Select the button
+let button = d3.select("#filter-btn");
+
+// Select the form
+let form = d3.select("#form");
+
+// Create the function to run for both events
+const runEnter = () => {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    let inputElement = d3.select("#date");
+
+    // Get the value property of the input element
+    // let inputValue = inputElement.property("value");
+    const value = inputElement.property("value")
+
+    // Print the value to the console
+    console.log(value);
+
+    //Use d3 to update each cell's text with
+    // weather report values (weekday, date, high, low)
+    tbody.selectAll("tr").remove();
+
+    data.forEach(item => {
+        if (item.datetime == value) {
+            let row = tbody.append("tr");
+            Object.values(item).forEach(value => {
+                // Append a cell to the row for each value
+                // in the weather report object
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        }
+    });
+}
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit", runEnter);
